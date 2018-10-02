16ce06c2da80f9d488acb14934e58ef93d94d5d6
vivek <kolianbolo@gmail.com>
2018-10-02 23:57:31 +0400

    some more ResisterService functional

--- weight-visualizer/src/main/kotlin/ru/bolo/service/RegisterService.kt
+++ weight-visualizer/src/main/kotlin/ru/bolo/service/RegisterService.kt
@@ -1,12 +1,18 @@
 package ru.bolo.service
 
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
+import ru.bolo.entity.User
+import ru.bolo.repository.UserRepository
 import ru.bolo.service.intf.IRegisterService
 
 @Service
 class RegisterService : IRegisterService {
 
+    @Autowired
+    lateinit var userRepository: UserRepository
+
     override fun register(login: String, password: String): Boolean {
-        TODO()
+        userRepository.save(User(login = login, password = password))
     }
 }
