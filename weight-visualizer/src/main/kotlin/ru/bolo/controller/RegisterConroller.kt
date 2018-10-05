bb7b0c257736014ee0c899670c7af6416545f154
vivek <kolianbolo@gmail.com>
2018-10-05 22:37:35 +0400

    Do hashing password

--- weight-visualizer/src/main/kotlin/ru/bolo/controller/RegisterConroller.kt
+++ weight-visualizer/src/main/kotlin/ru/bolo/controller/RegisterConroller.kt
@@ -6,6 +6,9 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import ru.bolo.model.RegisterModel
 import ru.bolo.service.intf.IRegisterService
+import java.nio.charset.StandardCharsets
+import java.security.MessageDigest
+
 
 @RestController
 @RequestMapping("/register")
@@ -14,11 +17,14 @@ class RegisterConroller {
     @Autowired
     lateinit var registerService: IRegisterService
 
+    var digest = MessageDigest.getInstance("SHA-256")
+
     @PostMapping
     fun register(model: RegisterModel) {
         val login: String = model.login ?: throw NullPointerException("login is null")
         val password: String = model.password ?: throw NullPointerException("password is null")
-        registerService.register(login, password)
+        val hash = digest.digest(password.toByteArray(StandardCharsets.UTF_8))
+        registerService.register(login, hash.contentToString())
     }
 
 }
